a590fa64fc4acc970dfec96664c5ee905a340918
Pan Teparak <panteparak@me.com>
2017-10-06 12:56:44 +0700

    awesome script, only list files not dir inside

--- awesome.sh
+++ awesome.sh
@@ -6,5 +6,5 @@ if ! [ ${1// } ] || [ "$1" == "--help" ]
 then
   echo "Usage: { ./awesome.sh size }"
 else
-  ls -l | awk -v size="$1" '$5 > size {print $9;}'
+  ls -l | grep '^-' | awk -v size="$1" '$5 > size {print $9;}'
 fi
